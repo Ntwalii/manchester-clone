e6b168d20526f939f94ea70f33ebbb25ba618466
ntwalii <ntwaliaubin@gmail.com>
2024-05-11 20:53:38 +0200

    Landing-page minus footer and sub-footer

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        bannerBrown:"82754F",
+        heroGray:"E1CCEB",
+        heroButton:"rgb(107,44,145)",
+        coursesBg:"rgb(107,44,145)",
+        coursesGold:"FFC983",
+        courseSearchButton:"383A3C",
+        toursBackground:"ECECED",
+        researchBackground:"ECECED"
+      }
+    },
+  },
+  plugins: [],
+}
+
